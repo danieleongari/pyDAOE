708eb20367a90b989d7181e63592597fffd96fe2
danieleongari <daniele.ongari@gmail.com>
2021-10-15 16:57:31 +0200

    add example 6.1

--- tests/test_chap6.py
+++ tests/test_chap6.py
@@ -13,3 +13,14 @@ def test_example_6_0():
     assert res.loc["A", "P-Value"] == 0.0000844371693000919
     assert res.loc["B", "P-Value"] == 0.0023615707965170714
     assert res.loc["AB", "P-Value"] == 0.18277648068045985
+
+
+def test_example_6_1():
+    """Example 6.1: 2^3 desgn on Plasma Etch experiment"""
+
+    df = pd.read_csv(DATA_DIR / "tab_6_4.csv")
+
+    res = twok_factorial(df)  # Compare with Table 6.6
+
+    assert res.loc["A", "P-Value"] == 0.002678610470970536
+    assert res.loc["ABC", "P-Value"] == 0.8185860578539553
