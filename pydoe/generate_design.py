181ece48e55e6915ca6cf4a43848eb875d4d34bc
danieleongari <daniele.ongari@gmail.com>
2021-10-16 02:11:22 +0200

    add more designs to be recognized

--- pydoe/generate_design.py
+++ pydoe/generate_design.py
@@ -1,17 +1,23 @@
 """Module for generating desing matrices."""
 
 
-def which_design(ntreats1, ntreats2=0, nblock1=0, nblock2=0, nblock3=0):
+def which_design(ntreats1, ntreats2=1, nblock1=1, nblock2=1, nblock3=1):
     """Return the name of the design one should run, given the specifics."""
-    if ntreats2 == 0:
-        if nblock1 == nblock2 == nblock3 == 0:
+    if ntreats2 == 1:
+        if nblock1 == nblock2 == nblock3 == 1:
             if ntreats1 == 2:
                 return "t_test"
             return "single_factor"
-        if nblock1 > 0 and nblock2 == nblock3 == 0:
+        if nblock1 > 1 and nblock2 == nblock3 == 1:
             return "rcbd"
-        if ntreats1 == nblock1 == nblock2 and nblock3 == 0:
+        if ntreats1 == nblock1 == nblock2 and nblock3 == 1:
             return "latin_square"
         if ntreats1 == nblock1 == nblock2 == nblock3:
             return "graeco_latin_square"
+    if ntreats1 == ntreats2 == 2 and nblock1 == nblock2 == nblock3 == 1:
+        return "twok_factorial"
+    if ntreats1 == ntreats2 == 3 and nblock1 == nblock2 == nblock3 == 1:
+        return "threek_factorial"
+    if ntreats1 > 1 and ntreats2 > 1 and nblock1 == nblock2 == nblock3 == 1:
+        return "two_factor_factorial"
     raise NotImplementedError("Not able to recognize the proper design for this problem.")
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -5,7 +5,7 @@ def test_which():
     """Test the function which design."""
     design = which_design(
         ntreats1=5,
-        ntreats2=0,
+        ntreats2=1,
         nblock1=5,
         nblock2=5,
         nblock3=5,
