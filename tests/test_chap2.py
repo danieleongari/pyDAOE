08429ad6b5a2d2b8d11c4ada2c654b7909015108
danieleongari <daniele.ongari@gmail.com>
2021-11-06 13:49:22 +0100

    implement Example 2.1

--- tests/test_chap2.py
+++ tests/test_chap2.py
@@ -11,3 +11,15 @@ def test_example_2_cement():
     res = t_test(df, equal_var=True, alternative='two-sided')
 
     assert res['P-Value'] == 0.0421967159248899
+
+
+def test_example_2_1():
+    """Example 2.1: nerve treatement results, assuming different variance and single tail.
+    Testing if the first treatement in the table (Nerve) leads to "greater" observation than the second (Muscle).
+    """
+
+    df = pd.read_csv(DATA_DIR / "tab_2_3.csv")
+
+    res = t_test(df, equal_var=False, alternative='greater')
+
+    assert res['P-Value'] == 0.0072776442038488855
