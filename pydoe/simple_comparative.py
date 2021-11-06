3fe8360ab50a03bc25285ac8860643625619a4da
danieleongari <daniele.ongari@gmail.com>
2021-11-06 14:02:11 +0100

    implement paired t-test with example

--- pydoe/simple_comparative.py
+++ pydoe/simple_comparative.py
@@ -20,3 +20,25 @@ def t_test(df, factor_col=None, observ_col=None, equal_var='True', alternative='
     )
 
     return {"P-Value": res.pvalue}
+
+
+def t_test_paired(df, factor_col=None, block_col=None, observ_col=None, alternative='two-sided'):
+    """Wrapper for scipy.stats.ttest_ind."""
+
+    # Default format of the dataframe
+    cols = [factor_col, block_col, observ_col]
+    if None in cols:
+        factor_col, block_col, observ_col = df.columns
+
+    treats = list(df[factor_col].unique())
+
+    # Sort by block so that the order of observations in a and b is correct (i.e., as paired)
+    df = df.sort_values(by=block_col)
+
+    res = stats.ttest_rel(
+        a=df[df[factor_col] == treats[0]].loc[:, observ_col].values,
+        b=df[df[factor_col] == treats[1]].loc[:, observ_col].values,
+        alternative=alternative  # default: 'two-sided'
+    )
+
+    return {"P-Value": res.pvalue}
--- tests/test_chap2.py
+++ tests/test_chap2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from pydoe.simple_comparative import t_test
+from pydoe.simple_comparative import t_test, t_test_paired
 from . import DATA_DIR
 
 
@@ -10,7 +10,7 @@ def test_example_2_cement():
 
     res = t_test(df, equal_var=True, alternative='two-sided')
 
-    assert res['P-Value'] == 0.0421967159248899
+    assert res['P-Value'] == 0.0421967159248899  # Compare with Table 2.2
 
 
 def test_example_2_1():
@@ -22,4 +22,16 @@ def test_example_2_1():
 
     res = t_test(df, equal_var=False, alternative='greater')
 
-    assert res['P-Value'] == 0.0072776442038488855
+    assert res['P-Value'] == 0.0072776442038488855  # 0.007 in the text
+
+
+def test_example_2_hardness():
+    """Example in the text about the hardness of a speciment, as measured for two tips.
+    Paired comparison design, with two-sided hypothesis.
+    """
+
+    df = pd.read_csv(DATA_DIR / "tab_2_6.csv")
+
+    res = t_test_paired(df, alternative='two-sided')
+
+    assert res['P-Value'] == 0.7976245209721027  # Compare with Table 2.7: null hypotesis NOT rejected!
