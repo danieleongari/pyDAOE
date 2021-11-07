0833bbc284f6b09d6eb725e547c62d0489d16bdc
danieleongari <daniele.ongari@gmail.com>
2021-11-07 12:25:00 +0100

    add Example 3.10

--- tests/test_chap3.py
+++ tests/test_chap3.py
@@ -11,3 +11,13 @@ def test_example_3_1():
     res = single_factor(df)  # Compare with Table 3.4
 
     assert res.loc["Treatements", "P-Value"] == 2.8828659084940823e-09
+
+
+def test_example_3_10():
+    """Example 3.10: Random Factor experiment on textile strength."""
+
+    df = pd.read_csv(DATA_DIR / "tab_3_18.csv")
+
+    res = single_factor(df)  # Compare with Table 3.19
+
+    assert res.loc["Treatements", "P-Value"] == 0.00018779198099318047  # <0.001 in the text
